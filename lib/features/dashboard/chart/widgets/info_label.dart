31c2ec0c6cff100c008998a7a9ad1179acd6f05b
zakir1999 <m.zakirhossen99@gmail.com>
2025-10-19 16:59:21 +0600

    update income overflow problem part

--- lib/features/dashboard/chart/widgets/info_label.dart
+++ lib/features/dashboard/chart/widgets/info_label.dart
@@ -14,23 +14,23 @@ class InfoLabel extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-          decoration: BoxDecoration(
-            color: color.withOpacity(0.1),
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Text(
-            "$label: $value",
-            style: TextStyle(
-              color: color,
-              fontWeight: FontWeight.w500,
-            ),
-          ),
+    return Flexible(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+        margin: const EdgeInsets.only(right: 8), // space between labels
+        decoration: BoxDecoration(
+          color: color.withOpacity(0.1),
+          borderRadius: BorderRadius.circular(10),
         ),
-      ],
+        child: Text(
+          "$label: $value",
+          style: TextStyle(
+            color: color,
+            fontWeight: FontWeight.w500,
+          ),
+          overflow: TextOverflow.ellipsis, // truncate long text with ...
+        ),
+      ),
     );
   }
 }
